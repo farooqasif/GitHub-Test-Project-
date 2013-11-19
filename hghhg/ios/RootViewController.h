225c1aac9058fe937ee857233c66606bca21150b
farooqasif <farooq@tintash.com>
2013-11-19 13:17:21 +0500

    test project
    test project testing github

---
+++ hghhg/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  hghhgAppController.h
+//  hghhg
+//
+//  Created by Farooq Asif on 19/11/2013.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
